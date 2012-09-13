431b34f76b56a232b28ea16e276dc2002d166f92
Jorge González <jorgonor88@gmail.com>
2012-09-13 23:03:18 +0200

    Configuration class is final

--- al095664/src/main/java/org/uji/agile/contactsbook/Configuration.java
+++ al095664/src/main/java/org/uji/agile/contactsbook/Configuration.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class Configuration {
+final class Configuration {
 	private static final String STORAGE_DIR_PROPERTY = "storage_dir";
 	private static final String PROPERTIES_FILE = "conf.properties";
 	private static Properties properties;
